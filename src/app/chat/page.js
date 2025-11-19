3c49b145aa91ac614e4e4785d7a2a5172c647f75
gtoovyrey <reymartcenteno03@gmail.com>
2025-11-19 21:27:16 +0800

    FixChatSystem2

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,14 +1,17 @@
+'use client';
+
 import { AblyProvider } from "ably/react";
 import Ably from "ably";
 import ChatClient from "./ChatClient";
+import { useMemo } from "react";
 
 // IMPORTANT: Create a .env.local file in your project's root
 // and add your new Ably API key like this:
 // ABLY_API_KEY=your-new-key
 
-const client = new Ably.Realtime({ authUrl: "/api/ably-token" });
-
 export default function ChatPage() {
+  const client = useMemo(() => new Ably.Realtime({ authUrl: "/api/ably-token" }), []);
+
   return (
     <AblyProvider client={client}>
         <h1 className="text-center my-4">Public Chat Room</h1>
@@ -16,8 +19,3 @@ export default function ChatPage() {
     </AblyProvider>
   );
 }
-
-export const metadata = {
-    title: "Chat",
-    description: "Live chat room",
-};
